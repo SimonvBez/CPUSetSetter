c84e488e0d8b8721e3e724c2b8487d5b77e3c45c
RegentScar <haydn.marco@gmail.com>
2025-11-07 22:42:53 +0100

    Use ObservableProperty in CoreUsage class

--- CPUSetSetter/UI/Tabs/Processes/CoreUsage/CoreUsage.cs
+++ CPUSetSetter/UI/Tabs/Processes/CoreUsage/CoreUsage.cs
@@ -2,22 +2,14 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace CPUSetSetter.UI.Tabs.Processes.CoreUsage
 {
-    public class CoreUsage(int coreIndex) : ObservableObject
+    public partial class CoreUsage(int coreIndex) : ObservableObject
     {
         public int CoreIndex { get; } = coreIndex;
 
+        [ObservableProperty]
         private double _usagePercent;
-        public double UsagePercent
-        {
-            get => _usagePercent;
-            set => SetProperty(ref _usagePercent, value);
-        }
 
+        [ObservableProperty]
         private bool _isParked;
-        public bool IsParked
-        {
-            get => _isParked;
-            set => SetProperty(ref _isParked, value);
-        }
     }
 }
